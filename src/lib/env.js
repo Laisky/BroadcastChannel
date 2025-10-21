fda6061c578acf2154754d3cc044e7bbd1ed3667
Laisky.Cai <github@laisky.com>
2025-10-21 21:18:52 +0000

    fix: improve getEnv function to handle undefined cases more gracefully

--- src/lib/env.js
+++ src/lib/env.js
@@ -1,3 +1,7 @@
+const _process = typeof require !== 'undefined' ? require('node:process') : undefined
+
 export function getEnv(env, Astro, name) {
-  return env[name] ?? Astro.locals?.runtime?.env?.[name]
+  return env?.[name]
+    ?? Astro?.locals?.runtime?.env?.[name]
+    ?? (_process ? _process.env?.[name] : undefined)
 }
